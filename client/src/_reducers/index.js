3af7e9c0b0d075568275a6ba3a6213e99dd827c9
unknown <arthursung98@gmail.com>
2021-07-14 22:15:00 -0500

    Notes on Class Components & Functional Components - React Hook

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,5 +1,6 @@
-// What is combineReducers? There can be many reducers, for user, subscribe, comment, or number.
-// The combineReducer puts everything together.
+// What is combineReducers? There can be many reducers, for instance 
+// user, subscribe, comment, or number. The combineReducer puts 
+// everything together.
 import { combineReducers } from 'redux'
 // import user from './user-reducer'
 
